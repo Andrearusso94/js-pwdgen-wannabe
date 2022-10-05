190a2df4e8b2f83b911b87f09e18a79d12b56ee7
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-05 14:23:39 +0200

    add color

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,7 @@ const myName = prompt('Il tuo Nome?')
 /* il mio cognome*/
 
 const secondName = prompt('Il tuo cognome?')
+
+/*il mio colore preferito*/
+
+const color = prompt ('Il tuo colore preferito?')
